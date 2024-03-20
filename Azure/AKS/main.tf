d3addeadd707aee8695cfeb8c0137c91a7faf5be
Thanachai Treratdilokkul <trthanachai@central.co.th>
2024-03-20 08:50:42 +0700

    update: Use new Module

--- Azure/AKS/main.tf
+++ Azure/AKS/main.tf
@@ -1,65 +1,48 @@
 provider "azurerm" {
   features {}
-  # skip_provider_registration = true
-  # subscription_id = "$SUBSCRIPTION_ID"
-  # client_id = "$CLIENT_ID"
-  # client_secret = "$CLIENT_SECRET"
-  # tenant_id = "$TENANT_ID"
 }
 
 terraform {
   backend "azurerm" {
-    # subscription_id = "$SUBSCRIPTION_ID"
-    # client_id = "$CLIENT_ID"
-    # client_secret = "$CLIENT_SECRET"
-    # tenant_id = "$TENANT_ID"
-    resource_group_name = "rg-sea-kanomnutt"
-    storage_account_name = "terraformstatekanomnutt"
-    container_name = "tfstate"
-    key = "az-aks.tfstate"
+    resource_group_name  = "rg-sea-kanomnutt"
+    storage_account_name = "stseakanomnutttf"
+    container_name       = "tfstate"
+    key                  = "az-aks.tfstate"
   }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.77.0"
-    }
-   azuread = {
-      source = "hashicorp/azuread"
-      version = "1.4.0"
+      version = "=3.85.0"
     }
   }
 }
 
-
-module "aks_cluster" {
-#  source = "./module/cluster"
-  source = "git@gitlab.com:thanachai.tre/terraform-module//aks/cluster/"
-  aks_name = var.aks_name
-  location = var.location
-  rg = var.rg
-  aks_version = var.aks_version
-  # sp_id = var.sp_id
-  # sp_secret = var.sp_secret
-  aks_agic_name = var.aks_agic_name
-  aks_agic_subnet = var.aks_agic_subnet
-
-  agent_pool_name = var.agent_pool_name
-  agent_pool_vm_size = var.agent_pool_vm_size
-  agnet_pool_subnet = var.agnet_pool_subnet
-  agnet_pool_pip = var.agnet_pool_pip
-  agent_pool_node_count = var.agent_pool_node_count
-  agent_pool_max_pods = var.agent_pool_max_pods
-  agent_pool_availability_zones = var.agent_pool_availability_zones
-  agnet_pool_only_critical_addons_enabled = var.agnet_pool_only_critical_addons_enabled
-  agent_pool_node_labels = var.agent_pool_node_labels
-  aks_tags = var.aks_tags
-
+module "az_aks_cluster" {
+  source                             = "../../Module/Azure/aks/cluster"
+  aks_name                           = var.aks_name
+  location                           = var.location
+  resource_group_name                = var.resource_group_name
+  kubernetes_version                 = var.kubernetes_version
+  sku_tier                           = var.sku_tier
+  enable_ingress_application_gateway = var.enable_ingress_application_gateway
+  image_cleaner_interval_hours       = var.image_cleaner_interval_hours
+  azure_ad                           = var.azure_ad
+  vnet_rg                            = var.vnet_rg
+  aks_vnet_name                      = var.aks_vnet_name
+  aks_subnet_name                    = var.aks_subnet_name
+  storage_profile                    = var.storage_profile
+  agent_pool                         = var.agent_pool
+  tags                               = var.tags
 }
 
-module "aks_worker" {
-#  source = "./module/node-pool"
-  source = "git@gitlab.com:thanachai.tre/terraform-module//aks/node-pool/"
-  aks_id = module.aks_cluster.kubernetes_cluster_id
-  worker_pool = var.aks_worker_pool
-  depends_on = [ module.aks_cluster ]
+module "az_aks_nodepool_stl" {
+  source          = "../../Module/Azure/aks/node-pool"
+  aks_name        = var.aks_name
+  aks_rg          = var.resource_group_name
+  aks_vnet_name   = var.aks_vnet_name
+  aks_subnet_name = var.aks_subnet_name
+  vnet_rg         = var.vnet_rg
+  node_pool       = var.node_pool_stl
+  tags            = var.tags
+  depends_on      = [module.az_aks_cluster]
 }
--- Azure/AKS/variables.tf
+++ Azure/AKS/variables.tf
@@ -1,69 +1,101 @@
 variable "aks_name" {
-    type = string  
+  type = string
 }
 variable "location" {
   type = string
 }
-variable "rg" {
+variable "resource_group_name" {
   type = string
 }
-variable "aks_version" {
+variable "kubernetes_version" {
   type = string
 }
-# variable "sp_id" {
-#   type = string
-# }
-# variable "sp_secret" {
-#   type = string
-# }
-variable "aks_agic_name" {
+variable "sku_tier" {
   type = string
 }
-variable "aks_agic_subnet" {
+variable "image_cleaner_interval_hours" {
+  type = number
+}
+variable "azure_ad" {
+  type = object({
+    rbac_managed      = bool
+    tenant_id         = string
+    admin_group       = list(string)
+    rbac_enabled      = bool
+    client_app_id     = string
+    server_app_id     = string
+    server_app_secret = string
+  })
+}
+variable "aks_subnet_name" {
   type = string
 }
-variable "agent_pool_name" {
+variable "aks_vnet_name" {
   type = string
 }
-variable "agent_pool_vm_size" {
-  type = string
+variable "storage_profile" {
+  type = object({
+    blob_driver_enabled         = optional(bool)
+    disk_driver_enabled         = optional(bool)
+    disk_driver_version         = optional(string)
+    file_driver_enabled         = optional(bool)
+    snapshot_controller_enabled = optional(bool)
+  })
 }
-variable "agnet_pool_subnet" {
-  type = string
+variable "agent_pool" {
+  type = object({
+    name                         = string
+    vm_size                      = string
+    os_sku                       = string
+    os_disk_size_gb              = number
+    enabled_pip                  = bool
+    enabled_auto_scale           = optional(bool)
+    node_count                   = number
+    node_max_count               = optional(number)
+    node_min_count               = optional(number)
+    max_pods                     = number
+    availability_zones           = set(string)
+    only_critical_addons_enabled = optional(bool)
+    node_labels                  = optional(map(string))
+  })
 }
-variable "agnet_pool_pip" {
-  type = bool
-}
-variable "agent_pool_node_count" {
-  type = string
-}
-variable "agent_pool_max_pods" {
-  type = string
-}
-variable "agent_pool_availability_zones" {
-  type = list
-}
-variable "agnet_pool_only_critical_addons_enabled" {
-  type = bool
-}
-variable "agent_pool_node_labels" {
-  type = map(string)
-}
-variable "aks_tags" {
+variable "tags" {
   type = map(string)
 }
 
-variable "aks_worker_pool" {
-  type = list(object({
-    worker_name = string
-    worker_vm_size = string
-    worker_node_count = string
-    worker_pip = bool
-    worker_max_pods = string
-    worker_aks_version = string
-    worker_subnet = string
-    worker_tags = map(string)
-    worker_availability_zones = list(string)
-    worker_labels = map(string)
-  }))
+variable "node_pool_stl" {
+  type = object({
+    name               = string
+    vm_size            = string
+    enabled_auto_scale = bool
+    node_count         = number
+    node_max_count     = optional(number)
+    node_min_count     = optional(number)
+    enabled_public_ip  = bool
+    max_pods           = number
+    mode               = string
+    availability_zones = set(string)
+    node_labels        = optional(map(string))
+    node_taints        = optional(list(string))
+    node_disk_size     = number
+    node_os_sku        = string
+  })
+}
+
+variable "vnet_rg" {
+  type = string
+}
+variable "enable_ingress_application_gateway" {
+  type    = bool
+  default = false
+}
+variable "ingress_application_gateway" {
+  type = object({
+    name      = string
+    subnet_id = string
+  })
+  default = {
+    name      = null
+    subnet_id = null
+  }
 }
